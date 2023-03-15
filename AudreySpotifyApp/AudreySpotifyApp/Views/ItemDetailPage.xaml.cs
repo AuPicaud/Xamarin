71f9e172cfe9d6dc2fc4ce7221818bfbd538c1de
AudreyPic <audrey.picaud@etu.unilim.fr>
2023-03-15 13:56:08 +0100

    Ajoutez des fichiers projet.

---
+++ AudreySpotifyApp/AudreySpotifyApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AudreySpotifyApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AudreySpotifyApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
